6e48901d6953dcd5d7bd056f3c4eba86c59ff563
0Standing0Man0 <ambarroy11@gmail.com>
2023-10-18 01:27:35 +0530

    Introducing bricks

--- src/BrickBreaker/Gameplay.java
+++ src/BrickBreaker/Gameplay.java
@@ -5,6 +5,8 @@ import java.awt.event.KeyListener;
 import javax.swing.Timer;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -21,10 +23,15 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     private int balldirX = -1;
     private int balldirY = -2;
 
+    private MapGenerator map;
+
     public Gameplay() {
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+
+        map=new MapGenerator(3,7);
+
         timer = new Timer(delay, this);
         timer.start();
     }
@@ -47,6 +54,8 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
         g.setColor(Color.yellow);
         g.fillOval(ballposX, ballposY, 20, 20);
 
+        map.draw((Graphics2D)g);
+
         g.dispose();
     }
 
@@ -54,8 +63,39 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
     public void actionPerformed(ActionEvent e) {
         timer.start();
         if (play) {
+
             ballposX += balldirX;
             ballposY += balldirY;
+
+            A: for(int i=0;i<map.map.length;i++){ // first map is from line 26 creating map object of MapGenerator
+                for(int j=0;j<map.map[0].length;j++){
+                    if(map.map[i][j]>0){
+                        int brickX = j* map.brickwidth + 80;
+                        int brickY = i* map.brickheight + 50;
+                        int brickwidth=map.brickwidth;
+                        int brickheight=map.brickheight;
+                        Rectangle brickrect = new  Rectangle(brickX, brickY, brickwidth, brickheight);
+                        Rectangle ballrect = new  Rectangle(ballposX, ballposX, balldirX, balldirY);
+                        if(ballrect.intersects(brickrect)){
+                            map.setBrickValue(0, i, j);
+                            totalbricks--;
+                            score+=5;
+                            if(ballposX+19>=brickrect.x || ballposX+1>=brickrect.x+brickrect.width){
+                                balldirX *= -1;
+                            }
+                            else{
+                                balldirY *= -1;
+                            }
+                            break A;
+                        }
+                    }
+                }
+            }
+
+            if(new Rectangle(ballposX,ballposY,20,20).intersects(new Rectangle(playerX, 550, 100, 8))){
+                balldirY *= -1;
+            }
+            
             if (ballposX < 0) {
                 balldirX *= -1;
             }
